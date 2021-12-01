853cf95b344ff05b9f961c442a9438495afacb76
sonder314 <86378184+sonder314@users.noreply.github.com>
2021-12-02 00:47:32 +0300

    Update Guessing game.py

--- Guessing game/Guessing game.py
+++ Guessing game/Guessing game.py
@@ -95,30 +95,37 @@ def word_result(letters, word):
 
 def play():
     word = get_word().upper()
-    res = ''
     tries = 6
     guessed_letters = ''
     print('Давайте играть в угадайку слов!')
+    print('Количество букв в слове:', len(word))
     print(len(word) * '_')
+    if input('Хотите увидеть первую и последнюю буквы слова? да/нет ') == 'да':
+        print(word[0] + len(word[1:-1]) * '_' + word[-1])
+        guessed_letters += word[0] + word[-1]
+    else:
+        print('Хорошо, тогда думайте своими мозгами... ')
     while tries != 0:
         print(display_hangman(tries))
         letter = input('Введите букву ')
-        if letter not in guessed_letters and letter in 'йцукенгшщзхъфывапролджэячсмитьбю' and len(str(letter)):
-            guessed_letters += letter
+        if letter.upper() not in guessed_letters and letter in 'йцукенгшщзхъфывапролджэячсмитьбю' and len(str(letter)) == 1:
+            guessed_letters += letter.upper()
             print(word_result(guessed_letters, word))
         else:
             print('Ваш символ не похож на русскую букву')
             continue
-        if input('Попробуйте угадать слово ').upper() != word:
-            print('Неправильно...', 'Попробуйте ещё раз', sep='\n')
-        else:
-            print('Поздравляем! Вы выиграли и получили самое бесценное, что у вас было и будет - жизнь!')
-            break
-        print(f'Оставшееся количесвто попыток {tries}')
+        if input('Уже готовы угадать слово? да/нет ').lower() == 'да':
+            if input('Попробуйте угадать слово ').upper() != word:
+                print('Неправильно...', 'Попробуйте ещё раз', sep='\n')
+            else:
+                print('Поздравляем! Вы выиграли и получили самое бесценное, что у вас было и будет - жизнь!')
+                break
         tries -= 1
-    print('Вы не смогли угадать слово и проиграли. Из-за вас, между прочим, повесили невинного человечка')
-
-play()
+        print(f'Оставшееся количесвто попыток {tries}')
+    else:
+        print(display_hangman(tries))
+        print('Вы не смогли угадать слово и проиграли. Из-за вас, между прочим, повесили невинного человечка')
+        print(f'А загаданное слово было {word}')
 
         
     
