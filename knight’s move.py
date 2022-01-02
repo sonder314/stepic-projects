151ca4db5e6bfb2ffe025bc6571edb7464e3643c
sonder314 <86378184+sonder314@users.noreply.github.com>
2022-01-02 17:30:48 +0300

    Update knight’s move.py

--- knight’s move.py
+++ knight’s move.py
@@ -1,20 +1,21 @@
-position = list(input())
-alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-chessboard = [[['.'] for _ in range(8)] for _ in range(8)]
-ind_i = 8 - int(position[1])
-ind_j = int(alph.index(position[0]))
+# knight’s move on the chessboard
+position = list(input())                                                 # take the knight's posotion
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']                          
+chessboard = [[['.'] for _ in range(8)] for _ in range(8)]               # сreate a chessboard
+index_i = 8 - int(position[1])                                           # convert indexes to type int
+index_j = int(alphabet.index(position[0]))
 operations = ['+2', '-2', '+1', '-1']
 
-chessboard[ind_i][ind_j] = 'N'
+chessboard[index_i][index_j] = 'N'                                       # mark the knight's position
 
-for i in operations[:2]:
+for i in operations[:2]:                                                 # match operations and indexes
     for j in operations[2:]:
-        if 0 <= ind_i + int(i) <= 7 and 0 <= ind_j + int(j) <= 7:
-            chessboard[ind_i + int(i)][ind_j + int(j)] = '*'
-        if 0 <= ind_i + int(j) <= 7 and 0 <= ind_j + int(i) <= 7:
-            chessboard[ind_i + int(j)][ind_j + int(i)] = '*'
+        if 0 <= index_i + int(i) <= 7 and 0 <= index_j + int(j) <= 7:    # mark the knight's move if it doesn't go   
+            chessboard[index_i + int(i)][index_j + int(j)] = '*'         # over the edge of the board
+        if 0 <= index_i + int(j) <= 7 and 0 <= index_j + int(i) <= 7:
+            chessboard[index_i + int(j)][index_j + int(i)] = '*'
 
-for i in chessboard:
+for i in chessboard:                                                     # display the chessboard
     for j in i:
         print(*j, end=' ')
     print()
